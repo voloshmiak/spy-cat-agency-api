23092a0a47d54e4ebdf82bcc39219804f9901f54
voloshmiak <voloshenkomykhailo@gmail.com>
2025-08-22 18:50:25 +0300

    Enhance logging middleware to capture response body in logs

--- internal/middleware/logging.go
+++ internal/middleware/logging.go
@@ -1,19 +1,35 @@
 package middleware
 
 import (
+	"bytes"
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"log"
 	"time"
 )
 
+type bodyLogWriter struct {
+	gin.ResponseWriter
+	body *bytes.Buffer
+}
+
+func (w bodyLogWriter) Write(b []byte) (int, error) {
+	w.body.Write(b)
+	return w.ResponseWriter.Write(b)
+}
+
 func Logger() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		blw := &bodyLogWriter{body: bytes.NewBufferString(""), ResponseWriter: c.Writer}
+		c.Writer = blw
+
 		t := time.Now()
 
 		c.Next()
 
-		log.Println(fmt.Sprintf("%s | %s | %d | %s", c.Request.Method, time.Since(t),
-			c.Writer.Status(), c.Request.URL.Path))
+		statusCode := c.Writer.Status()
+
+		log.Println(fmt.Sprintf("%s | %s | %d | %s | %s", c.Request.Method, time.Since(t),
+			statusCode, c.Request.URL.Path, blw.body.String()))
 	}
 }
